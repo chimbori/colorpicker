34c5f743c562f5891be99c3634a37d4026970921
Chimbori <hello@chimbori.com>
2024-12-05 12:23:28 -0800

    Add standard `.editorconfig` and re-format all the sources

--- app/src/main/java/com/flask/colorpicker/sample/PrefsActivity.java
+++ app/src/main/java/com/flask/colorpicker/sample/PrefsActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class PrefsActivity extends PreferenceActivity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		addPreferencesFromResource(R.xml.prefs);
-	}
+    addPreferencesFromResource(R.xml.prefs);
+  }
 }
